e0dd08af8264156c0daa4a65bf9d846d48aee85a
angelique_rosin <angie1979.capucine@yahoo.fr>
2022-10-23 22:59:28 +0200

    mise en place du slider

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const hamburgerToggler = document.querySelector('.hamburger');
-console.log(hamburgerToggler);
+// console.log(hamburgerToggler);
 const navLinksContainer = document.querySelector('.navlinks-container')
-console.log(navLinksContainer)
+// console.log(navLinksContainer)
 
 const toggleNav = () => {
     hamburgerToggler.classList.toggle('open');
@@ -10,3 +10,40 @@ const toggleNav = () => {
     
 };
 hamburgerToggler.addEventListener('click', toggleNav);
+
+/*******************Slider*******************/
+
+/**
+ * Je pointe toutes les images et obtiens un HTML collection
+ */
+let imageSlider = document.getElementsByClassName('image-slider')
+console.log(imageSlider);
+
+
+// let imageSlider = document.querySelectorAll('.image-slider')
+// console.log(imageSlider);
+ let etape = 0; // c'est la 1ere image
+
+ /**
+  * On a besoin du nombre d'images
+  */
+ nombreImages = imageSlider.length;
+ /**
+  * On parcourt le tableau imageSlider, et on enlève toutes les classes 'actives' sur toutes les images
+  */
+ function removeActiveImage () {
+    for (let i = 0; i < nombreImages; i++) {
+        imageSlider[i]. classList.remove('active')
+    }
+ }
+
+ setInterval(function() {
+    etape++;
+    if(etape >= nombreImages) {
+        etape = 0;
+    }
+    removeActiveImage();
+    imageSlider[etape].classList.add('active')
+ }, 5000);
+
+
