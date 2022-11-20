646784ba6157a41049193b2e7a55a5fde6c1a543
angelique_rosin <angie1979.capucine@yahoo.fr>
2022-11-20 23:03:38 +0100

    mise en place popo up

---
+++ src/popUp.js
@@ -0,0 +1,26 @@
+/**
+ * Faire apparaitre la popup au scroll vertical
+*/
+const scrollPopUp = () => {
+//pour avoir la taille de la hauteur du body
+//console.log(document.body.offsetHeight);
+
+//comme la taille du body s'allonge en rétrécissant l'écan, il faut cette formule :
+    let scrollValue = (window.scrollY + window.innerHeight) / document.body.offsetHeight;
+
+    if(scrollValue > 0.85){
+        popup.style.opacity = 1;
+        popup.style.transform = 'none';
+    }
+}
+window.addEventListener('scroll', scrollPopUp);
+
+/**
+ * Fermer la pop up
+ */
+const closePopUp = () =>{
+    popup.style.opacity = 0;
+    popup.style.transform = 'translateX(500px)';
+}
+
+closeBtn.addEventListener('click', closePopUp);
